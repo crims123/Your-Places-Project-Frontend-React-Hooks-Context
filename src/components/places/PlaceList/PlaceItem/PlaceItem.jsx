fa4301555a556a8ae7887317e2052ea7c09fb1fc
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-19 09:51:34 -0500

    Added Modal to PlaceItem

--- src/components/places/PlaceList/PlaceItem/PlaceItem.jsx
+++ src/components/places/PlaceList/PlaceItem/PlaceItem.jsx
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PlaceItem.css';
 import Card from '../../../shared/Card';
 import Button from '../../../shared/Button';
+import Modal from '../../../shared/Modal';
 
 const PlaceItem = ({ id, image, title, description, address }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={image} alt={title} />
+    <React.Fragment>
+      <Modal
+        show={showModal}
+        onCancel={handleCloseModal}
+        header={address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={handleCloseModal}>CLOSE</Button>}
+      >
+        <div className="map-container">
+          <h2>THE MAP!</h2>
         </div>
-        <div className="place-item__info">
-          <h2>{title}</h2>
-          <h3>{address}</h3>
-          <p>{description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button>VIEW ON MAP</Button>
-          <Button to={`places/${id}`}>EDIT</Button>
-          <Button>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={image} alt={title} />
+          </div>
+
+          <div className="place-item__info">
+            <h2>{title}</h2>
+            <h3>{address}</h3>
+            <p>{description}</p>
+          </div>
+
+          <div className="place-item__actions">
+            <Button onClick={handleOpenModal}>VIEW ON MAP</Button>
+            <Button to={`places/${id}`}>EDIT</Button>
+            <Button>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </React.Fragment>
   );
 };
 
