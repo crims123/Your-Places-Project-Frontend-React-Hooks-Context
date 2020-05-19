73b1b609eb3c7c9f53ce76a3aa5fd91164b9cc34
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-19 08:30:49 -0500

    Added user places

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/shared/Header';
 import UsersList from './components/users/UsersList';
+import PlaceList from './components/places/PlaceList';
 import NewPlace from './components/places/NewPlace';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <main>
         <Switch>
           <Route exact path="/" component={UsersList} />
+          <Route exact path="/:userId/places" component={PlaceList} />
           <Route exact path="/places/new" component={NewPlace} />
         </Switch>
       </main>
---
+++ src/components/places/PlaceList/PlaceList.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import './PlaceList.css';
+import Card from '../../shared/Card';
+import PlaceItem from './PlaceItem';
+
+const places = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Emp. State Building',
+    description: 'One of the most famous sky scrapers in the world!',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u2',
+  },
+];
+
+const PlaceList = () => {
+  if (!places.length) {
+    return (
+      <div className="place-list center">
+        <Card>
+          <h2>No places found. Maybe create one?</h2>
+          <button to="/places/new">Create Place</button>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="place-list">
+      {places.map(
+        ({ id, imageUrl, title, description, address, creator, location }) => (
+          <PlaceItem
+            key={id}
+            id={id}
+            image={imageUrl}
+            title={title}
+            description={description}
+            address={address}
+            creatorId={creator}
+            coordinates={location}
+          />
+        )
+      )}
+    </ul>
+  );
+};
+
+export default PlaceList;
