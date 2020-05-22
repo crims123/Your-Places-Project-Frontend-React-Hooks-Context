bd52440b0170c3608b34a99abb7ee6f68c95d406
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-22 08:59:57 -0500

    Added useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from 'react';
+import axiosClient from '../config/axios';
+
+const useFetch = (url, verb, values) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  let client = null;
+
+  switch (verb) {
+    case 'get':
+      client = axiosClient.get(url);
+      break;
+
+    case 'post':
+      client = axiosClient.post(url, values);
+      break;
+
+    case 'put':
+      client = axiosClient.put(url, values);
+      break;
+
+    case 'patch':
+      client = axiosClient.patch(url, values);
+      break;
+
+    case 'delete':
+      client = axiosClient.delete(url);
+      break;
+
+    default:
+      client = axiosClient.get(url);
+      break;
+  }
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        setIsLoading(true);
+        const response = await client;
+        setData(response.data.data);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        setError(error.response.data.message);
+      }
+    };
+    fetch();
+  }, []);
+
+  const handleError = () => {
+    setError(null);
+  };
+
+  return [data, isLoading, error, handleError];
+};
+
+export default useFetch;
