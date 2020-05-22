767e92bdc57d1c6d5027c9f0f921edf000039c9b
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-22 07:28:05 -0500

    Added login to the server

--- src/components/users/Auth/Login/Login.jsx
+++ src/components/users/Auth/Login/Login.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect, useContext } from 'react';
+import axiosClient from '../../../../config/axios';
 import './Login.css';
 import { AuthContext } from '../../../../context/auth-context';
 import Input from '../../../shared/Input';
 import Button from '../../../shared/Button';
 import Card from '../../../shared/Card';
+import ErrorModal from '../../../shared/ErrorModal';
+import LoadingSpinner from '../../../shared/LoadingSpinner';
 import useInput from '../../../../hooks/useInput';
 
 const Login = (props) => {
   const { setIsAuth } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const [values, handleOnChange] = useInput({});
   useEffect(() => {
@@ -21,45 +26,61 @@ const Login = (props) => {
 
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
-  const handleLoginSubmit = (e) => {
+  const handleLoginSubmit = async (e) => {
     e.preventDefault();
-    console.log(values); // send to the backend
-    setIsAuth(true);
-    props.history.push('/')
+    try {
+      setIsLoading(true);
+      const response = await axiosClient.post('/api/users/login', values);
+      console.log(response)
+      setIsLoading(false);
+      setIsAuth(true);
+      props.history.push('/');
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.response.data.message);
+    }
+  };
+
+  const handleError = () => {
+    setError(null);
   };
 
   return (
-    <Card className="authentication">
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={handleLoginSubmit}>
-        <Input
-          id="email"
-          type="email"
-          element="input"
-          label="Email"
-          handleOnChange={handleOnChange}
-          name="email"
-        />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={handleError} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverLay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={handleLoginSubmit}>
+          <Input
+            id="email"
+            type="email"
+            element="input"
+            label="Email"
+            handleOnChange={handleOnChange}
+            name="email"
+          />
 
-        <Input
-          id="password"
-          type="password"
-          element="input"
-          label="Password"
-          handleOnChange={handleOnChange}
-          name="password"
-        />
+          <Input
+            id="password"
+            type="password"
+            element="input"
+            label="Password"
+            handleOnChange={handleOnChange}
+            name="password"
+          />
 
-        <Button type="submit" disabled={buttonDisabled}>
-          LOGIN
+          <Button type="submit" disabled={buttonDisabled}>
+            LOGIN
+          </Button>
+        </form>
+
+        <Button inverse to="/signup">
+          SWITCH TO SIGNUP
         </Button>
-      </form>
-
-      <Button inverse to="/signup">
-        SWITCH TO SIGNUP
-      </Button>
-    </Card>
+      </Card>
+    </React.Fragment>
   );
 };
 
