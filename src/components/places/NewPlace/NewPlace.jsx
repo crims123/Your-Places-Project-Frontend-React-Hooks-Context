2ba722a4d8774173490b9ec8a1d67d9506c5960c
Cristian David Pinto Penaloza <cristiandavid.pinto@globant.com>
2020-05-22 10:56:24 -0500

    Added add place to server

--- src/components/places/NewPlace/NewPlace.jsx
+++ src/components/places/NewPlace/NewPlace.jsx
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react';
 import './NewPlace.css';
 import Input from '../../shared/Input';
 import Button from '../../shared/Button';
+import ErrorModal from '../../shared/ErrorModal';
+import LoadingSpinner from '../../shared/LoadingSpinner';
 import useInput from '../../../hooks/useInput';
+import useFetchOnSubmit from '../../../hooks/useFetchOnSumbit';
 
-const NewPlace = () => {
+const NewPlace = (props) => {
+  const [fetchData, isLoading, error, handleError] = useFetchOnSubmit();
+
   const [values, handleOnChange] = useInput({});
   useEffect(() => {
     const { title, description, address } = values;
@@ -17,46 +22,53 @@ const NewPlace = () => {
 
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
-  const handleNewPlaceSubmit = (e) => {
+  const handleNewPlaceSubmit = async (e) => {
     e.preventDefault();
-    console.log(values); // send to the backend
+    const response = await fetchData('/api/places', 'post', values);
+    if (response) {
+      props.history.push('/');
+    }
   };
 
   return (
-    <form className="place-form" onSubmit={handleNewPlaceSubmit}>
-      <Input
-        id="title"
-        type="text"
-        placeholder="Add Title"
-        element="input"
-        label="Title"
-        handleOnChange={handleOnChange}
-        name="title"
-      />
+    <React.Fragment>
+      <ErrorModal error={error} onClear={handleError} />
+      {isLoading && <LoadingSpinner asOverLay />}
+      <form className="place-form" onSubmit={handleNewPlaceSubmit}>
+        <Input
+          id="title"
+          type="text"
+          placeholder="Add Title"
+          element="input"
+          label="Title"
+          handleOnChange={handleOnChange}
+          name="title"
+        />
 
-      <Input
-        id="description"
-        type="text"
-        placeholder="Add Description"
-        label="Description"
-        handleOnChange={handleOnChange}
-        name="description"
-      />
+        <Input
+          id="description"
+          type="text"
+          placeholder="Add Description"
+          label="Description"
+          handleOnChange={handleOnChange}
+          name="description"
+        />
 
-      <Input
-        id="address"
-        type="text"
-        placeholder="Add Address"
-        element="input"
-        label="Address"
-        handleOnChange={handleOnChange}
-        name="address"
-      />
+        <Input
+          id="address"
+          type="text"
+          placeholder="Add Address"
+          element="input"
+          label="Address"
+          handleOnChange={handleOnChange}
+          name="address"
+        />
 
-      <Button type="submit" disabled={buttonDisabled}>
-        Create
-      </Button>
-    </form>
+        <Button type="submit" disabled={buttonDisabled}>
+          Create
+        </Button>
+      </form>
+    </React.Fragment>
   );
 };
 
